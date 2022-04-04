7e6801b2629501f9856d82443a316f9327411987
Stewart Morgan <stewartmorgan86@gmail.com>
2022-04-04 23:15:22 +0100

    updated to npm logging code

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const app = express();
-const morgan = require("morgan");
-const winston = require("winston");
-const CloudWatchTransport = require("winston-aws-cloudwatch");
-const jwt = require("jsonwebtoken");
+const AWSSettings = require("./config/database").settings;
+const apiname = process.env.API_NAME || "API";
+const APILogging = require("diu-api-logging").Methods.Logging.APILogging;
 
 // SWAGGER SETUP
 const swaggerJSDoc = require("swagger-jsdoc");
@@ -35,78 +34,8 @@ const swaggerSpec = swaggerJSDoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // ERROR LOGGER SETUP
-morgan.token("token", function (req) {
-  if (req.headers.authorization) {
-    return JSON.stringify(jwt.decode(req.headers.authorization.replace("JWT ", "")));
-  } else {
-    return "{}";
-  }
-});
-const apiname = process.env.API_NAME || "API";
-const addAppNameFormat = winston.format((info) => {
-  info.appName = apiname;
-  return info;
-});
-const parseMessage = function (message) {
-  const variables = message.split("|");
-  const token = JSON.parse(variables[6]);
-  let username = "";
-  if (token && token.username) username = token.username;
-  let organisation = "";
-  if (token && token.organisation) organisation = token.organisation;
-  return {
-    method: variables[0],
-    url: variables[1],
-    status: variables[2],
-    response: variables[3],
-    responsetime: variables[4],
-    date: variables[5],
-    token: {
-      username: username,
-      organisation: organisation,
-    },
-  };
-};
-const AWSSettings = require("./config/database").settings;
-const awstransport = new CloudWatchTransport({
-  logGroupName: "api-calls",
-  logStreamName: apiname,
-  createLogGroup: false,
-  createLogStream: true,
-  submissionInterval: 2000,
-  submissionRetryCount: 1,
-  batchSize: 1,
-  awsConfig: {
-    accessKeyId: AWSSettings.accessKeyId,
-    secretAccessKey: AWSSettings.secretAccessKey,
-    region: AWSSettings.awsregion,
-  },
-  formatLog: (item) => `${item.level}: ${item.message} ${JSON.stringify(item.meta)}`,
-});
-// @ts-ignore
-const logger = new winston.createLogger({
-    transports: [awstransport],
-    format: winston.format.combine(addAppNameFormat(), winston.format.json()),
-    // @ts-ignore
-    dynamicMeta: function (req, res, err) {
-      return req.header;
-    },
-    meta: true,
-    expressFormat: true,
-    colorize: false,
-  }),
-  loggerstream = {
-    write: function (message, encoding) {
-      logger.info(message.replace(/\n$/, ""), {
-        metaData: parseMessage(message.replace(/\n$/, "")),
-      });
-    },
-  };
-app.use(
-  morgan(":method|:url|:status|:res[content-length]|:response-time|:date[iso]|:token", {
-    stream: loggerstream,
-  })
-);
+const logs = APILogging(apiname, AWSSettings);
+app.use(logs);
 
 // SETTINGS FOR OUR API
 // =============================================================================
