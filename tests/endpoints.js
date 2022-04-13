22b1f0798adeb90d8b52014a7a1f221d96b30c5a
Luke Tinnion <luke.tinnion@nhs.net>
2022-04-13 16:22:11 +0100

    Added all endpoints for testing

--- tests/endpoints.js
+++ tests/endpoints.js
@@ -1,5 +1,195 @@
 module.exports.SecureEndpoints = [
     {
+        url: `/access-logs`,
+        type: "JWT",
+        method: "get",
+    },
+    // {
+    //     url: `/{user}/access-logs`,
+    //     type: "JWT",
+    //     method: "get",
+    // },
+    {
+        url: `/access-logs/statistics`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/access-logs/create`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/capabilities`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/register`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/capabilities/update`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/capabilities/removeById`,
+        type: "JWT",
+        method: "delete",
+    },
+    {
+        url: `/capabilities/getById`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/getByTag`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/getByTagsAnd`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/getByTagsOr`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/links/sync`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/capabilities/links/create`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/capabilities/links/delete`,
+        type: "JWT",
+        method: "delete",
+    },
+    {
+        url: `/capabilities/getByRoleName`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/getByTeamIds`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/capabilities/getAllCapabilitiesWithTeamAndUsername`,
+        type: "JWT",
+        method: "post",
+    },
+    // {
+    //     url: `/{type}/{id}/capabilities`,
+    //     type: "JWT",
+    //     method: "get",
+    // },
+    {
+        url: `/confluence/content/search`,
+        type: null,
+        method: "get",
+    },
+    // {
+    //     url: `/confluence/content/{id}`,
+    //     type: null,
+    //     method: "get",
+    // },
+    {
+        url: `/demographics/demographicsbynhsnumber`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/demographics/validateNHSNumber`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/demographics/findMyNHSNumber`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/docobo/acknowledgements/getAll`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/docobo/acknowledgements/report`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/docobooutbound/getpatientsbyorg`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/docobooutbound/getpatientdata`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/docobooutbound/processDocboInfo`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/gpinpatients/authenticate`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/inpatientcounts`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/outpatientcounts`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/aecounts`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/ecscounts`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/epccounts`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/inpatientgpsummary`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/gpinpatients/aegpsummary`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/lpresviewer/getValidationKey`,
+        type: "JWT",
+        method: "post",
+    },
+    {
         url: `/mfa/register`,
         type: "JWT",
         method: "get",
@@ -25,103 +215,28 @@ module.exports.SecureEndpoints = [
         method: "get",
     },
     {
-        url: `/serviceaccounts/check`,
-        type: null,
-        method: "post",
-    },
-    {
-        url: `/teamroles/getItemsByTeamcode`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/teamroles/register`,
-        type: "JWT",
-        method: "post",
-    },
-    {
-        url: `/teamroles/remove`,
-        type: "JWT",
-        method: "post",
-    },
-    {
-        url: `/teamroles/getAll`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/userroles/getItemsByUsername`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/userroles/register`,
-        type: "JWT",
-        method: "post",
-    },
-    {
-        url: `/userroles/remove`,
-        type: "JWT",
-        method: "post",
-    },
-    {
-        url: `/userroles/getAll`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/users/register`,
-        type: null,
-        method: "post",
-    },
-    {
-        url: `/users/profile`,
-        type: "JWT",
-        method: "get",
-    },
-    {
         url: `/otp/validate`,
         type: "JWT",
         method: "post",
     },
     {
-        url: `/users/authenticate`,
+        url: `/niceevidence/evidencesearch`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/opensource/getByPage`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/opensource/addView`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/password/update`,
         type: null,
-        method: "post",
-    },
-    {
-        url: `/users/validate`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/userroles/changemytrainingsystemrole`,
-        type: "JWT",
-        method: "post",
-    },
-    {
-        url: `/pcninformation/getData`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/postcodes/getAll`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/demographics/demographicsbynhsnumber`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/demographics/validateNHSNumber`,
-        type: "JWT",
-        method: "post",
-    },
-    {
-        url: `/demographics/findMyNHSNumber`,
-        type: "JWT",
         method: "post",
     },
     {
@@ -140,10 +255,125 @@ module.exports.SecureEndpoints = [
         method: "get",
     },
     {
-        url: `/shielding/getCitizens`,
+        url: `/pcninformation/getData`,
         type: "JWT",
         method: "get",
     },
+    {
+        url: `/postcodes/getAll`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/requests`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/requests/account`,
+        type: null,
+        method: "post",
+    },
+    // {
+    //     url: `/requests/account/{id}`,
+    //     type: null,
+    //     method: "post",
+    // },
+    {
+        url: `/requests/account/complete`,
+        type: null,
+        method: "post",
+    },
+    {
+        url: `/teamrequests/register`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/teamrequests/archive`,
+        type: "JWT",
+        method: "put",
+    },
+    {
+        url: `/teamrequests/update`,
+        type: "JWT",
+        method: "put",
+    },
+    {
+        url: `/teamrequests/getByID`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/teamrequests/getAll`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/teamrequests/getRequestsByUsername`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/teamrequests/getRequestsByTeamCode`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/teamrequests/getOutstandingRequests`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/teamrequests/getOutstandingRequests`,
+        type: "JWT",
+        method: "get",
+    },
+    // {
+    //     url: `/{type}/{id}/roles`,
+    //     type: "JWT",
+    //     method: "get",
+    // },
+    {
+        url: `/roles`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/roles/create`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/roles/update`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/roles/links/sync`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/roles/links/create`,
+        type: "JWT",
+        method: "post",
+    },
+    {
+        url: `/roles/links/delete`,
+        type: "JWT",
+        method: "delete",
+    },
+    // {
+    //     url: `/roles/{id}`,
+    //     type: "JWT",
+    //     method: "get",
+    // },
+    // {
+    //     url: `/roles/{id}/delete`,
+    //     type: "JWT",
+    //     method: "delete",
+    // },
     {
         url: `/searchs/searchTeams`,
         type: "JWT",
@@ -156,6 +386,16 @@ module.exports.SecureEndpoints = [
     },
     {
         url: `/searchusers/searchOrgUserProfiles`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/serviceaccounts/check`,
+        type: null,
+        method: "post",
+    },
+    {
+        url: `/shielding/getCitizens`,
         type: "JWT",
         method: "get",
     },
@@ -230,44 +470,34 @@ module.exports.SecureEndpoints = [
         method: "put",
     },
     {
-        url: `/teamrequests/register`,
+        url: `/teams`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/teams/create`,
         type: "JWT",
         method: "post",
     },
     {
-        url: `/teamrequests/archive`,
+        url: `/teams/update`,
         type: "JWT",
-        method: "put",
+        method: "post",
     },
+    // {
+    //     url: `/teams/{id}/delete`,
+    //     type: "JWT",
+    //     method: "delete",
+    // },
     {
-        url: `/teamrequests/update`,
-        type: "JWT",
-        method: "put",
-    },
-    {
-        url: `/teamrequests/getByID`,
+        url: `/trials/getAll`,
         type: "JWT",
         method: "get",
     },
     {
-        url: `/teamrequests/getAll`,
+        url: `/trials/getSearchTop1000`,
         type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/teamrequests/getRequestsByUsername`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/teamrequests/getRequestsByTeamCode`,
-        type: "JWT",
-        method: "get",
-    },
-    {
-        url: `/teamrequests/getOutstandingRequests`,
-        type: "JWT",
-        method: "get",
+        method: "post",
     },
     {
         url: `/userprofiles/register`,
@@ -294,4 +524,49 @@ module.exports.SecureEndpoints = [
         type: "JWT",
         method: "put",
     },
+    {
+        url: `/users/register`,
+        type: null,
+        method: "post",
+    },
+    {
+        url: `/users/profile`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/users/authenticate`,
+        type: null,
+        method: "post",
+    },
+    {
+        url: `/users/validate`,
+        type: "JWT",
+        method: "get",
+    },
+    {
+        url: `/users/delete`,
+        type: "JWT",
+        method: "delete",
+    },
+    {
+        url: `/virtualward/getAll`,
+        type: null,
+        method: "get",
+    },
+    {
+        url: `/virtualward/update`,
+        type: null,
+        method: "post",
+    },
+    {
+        url: `/users/send-code`,
+        type: null,
+        method: "post",
+    },
+    {
+        url: `/users/verify-code`,
+        type: null,
+        method: "post",
+    }
 ];
